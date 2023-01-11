7320f8ca2f84e49794ff190c1af3d00a4d6fab2c
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-11 10:35:21 +0430

    Grand parent renders the parentone and childone both because the props change in grand parent make the parent one to re-render and childone too but the childone cannot go through the commit phase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import './App.css';
-import { ChildOne } from './Components/Parent Child/ChildOne';
-import ParentOne from './Components/Parent Child/ParentOne';
+import { GrandParent } from './Components/Parent Child/GrandParent';
+
 
 
 function App() {
   return (
     <div className="App">
-        <ParentOne>
-          <ChildOne />
-        </ParentOne>
+        <GrandParent />
     </div>
   );
 }
